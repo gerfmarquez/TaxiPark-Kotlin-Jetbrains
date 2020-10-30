08d5e7e55102c048333e4f9b83d1dae0c6958ddb
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-10-29 23:13:19 -0400

    optimized and simplified code

--- Nice String/Task/src/nicestring/NiceString.kt
+++ Nice String/Task/src/nicestring/NiceString.kt
@@ -3,29 +3,11 @@ package nicestring
 fun String.isNice(): Boolean {
 
 
-    var firstCondition = 1//true
-    var secondCondition = 0//false
-    var thirdCondition = 0//false
+    val noBadSubstring = setOf("bu","ba","be").none{ this.contains(it) }
 
-    if(this.contains(Regex("bu")) || this.contains(Regex("ba")) ||
-            this.contains(Regex("be"))) {
-        firstCondition = 0//false
-    }
-    val vowels = "aeiou".sumBy { ch->
-        this.count{ ch == it }
-    }
-    if(vowels >= 3) {
-        secondCondition = 1//true
-    }
+    val hasThreeVowels = count { it in "aeiou" } >= 3
 
-    val doubleLetter = this.zipWithNext()
-            .count { pair ->
-                (pair.first == pair.second)
-            }
-    if(doubleLetter >= 1) {
-        thirdCondition = 1//true
-    }
+    val hasDoubleLetter = this.zipWithNext().any{ it.first == it.second }
 
-    var total = firstCondition + secondCondition + thirdCondition
-    return (total in 2..3)
+    return listOf(noBadSubstring,hasThreeVowels,hasDoubleLetter).count{ it } >= 2
 }
