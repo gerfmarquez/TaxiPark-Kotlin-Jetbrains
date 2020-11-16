603b81c16587812bfc9c5fb74c4c470efbf66adf
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:24:40 -0500

    license

--- Nice String/Task/src/nicestring/NiceString.kt
+++ Nice String/Task/src/nicestring/NiceString.kt
@@ -2,6 +2,11 @@ package nicestring
 
 fun String.isNice(): Boolean {
 
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
+ */
 
     val noBadSubstring = setOf("bu","ba","be").none{ this.contains(it) }
 
